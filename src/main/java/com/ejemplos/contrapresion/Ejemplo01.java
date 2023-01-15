f524a8640bedef475a871d9c731595aa4459974d
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-01-15 14:47:04 -0500

    Backpressure

---
+++ src/main/java/com/ejemplos/contrapresion/Ejemplo01.java
@@ -0,0 +1,17 @@
+package com.ejemplos.contrapresion;
+
+import reactor.core.publisher.Flux;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Ejemplo01 {
+    public static void main(String[] args) {
+
+        Flux<String> ciudades = Flux.fromIterable(
+                new ArrayList<>(Arrays.asList("New York", "London", "Paris", "Toronto", "Rome"))
+        );
+
+        ciudades.log().subscribe();
+
+    }
+}
