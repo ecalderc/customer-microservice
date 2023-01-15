633bfd64b0c8df4031e89abe8f6e728d84f6b158
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-01-15 14:33:57 -0500

    Adding example services

--- src/test/java/com/ejemplos/servicios/ServicioSencilloTest.java
+++ src/test/java/com/ejemplos/servicios/ServicioSencilloTest.java
@@ -7,6 +7,8 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
+
 @SpringBootTest(classes=ServicioSencillo.class)
 class ServicioSencilloTest {
 
@@ -23,6 +25,17 @@ class ServicioSencilloTest {
     void buscarTodosTest() {
         Flux<String> todos = servicioSencillo.buscarTodos();
         StepVerifier.create(todos).expectNext("Hola").expectNext("Mundo").verifyComplete();
+    }
+
+    @Test
+    void buscarTodosLentoTest() {
+        Flux<String> todos = servicioSencillo.buscarTodosLento();
+        StepVerifier.create(todos)
+                .expectNext("Hola")
+                .thenAwait(Duration.ofSeconds(1))
+                .expectNext("Mundo")
+                .thenAwait(Duration.ofSeconds(1))
+                .verifyComplete();
     }
 
 }
