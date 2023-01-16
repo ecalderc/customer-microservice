aed42a7949406a929cb2d14ef76bf233f234fb86
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-01-16 08:07:37 -0500

    removing examples

--- src/main/java/com/ejemplos/excepciones/CustomException.java
+++
@@ -1,7 +0,0 @@
-package com.ejemplos.excepciones;
-
-public class CustomException extends Exception{
-    public CustomException (String message, Throwable exception){
-        super(message, exception);
-    }
-}
