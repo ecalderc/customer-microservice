5bfb1d68dd79b08d9c09eac1d3341a141d4ce907
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-01-16 17:23:42 -0500

    Adding swagger

--- src/main/java/com/nttdata/bootcamp/customer/CustomerApplication.java
+++ src/main/java/com/nttdata/bootcamp/customer/CustomerApplication.java
@@ -4,8 +4,12 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "Customer API", version = "1.0", description = "Documentation Customer API v1.0")
+)
 public class CustomerApplication {
 
 	public static void main(String[] args) {
