e57e51e6a0aa09d9fa510c57be97c55978a0175d
Eber Calderon <67397815+ebercalderon@users.noreply.github.com>
2023-01-19 16:48:54 -0500

    Validations updates

--- src/main/java/com/nttdata/bootcamp/customer/CustomerApplication.java
+++ src/main/java/com/nttdata/bootcamp/customer/CustomerApplication.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @OpenAPIDefinition(info =
@@ -12,8 +11,8 @@ import org.springframework.context.annotation.ComponentScan;
 )
 public class CustomerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
 
 }
